1eabee5d25830487e3a73e6d901f118e1853e8ce
galileoguzman <galileoguzman@gmail.com>
2020-08-15 14:22:36 -0500

    Added exercise

--- funciones_1_filter.py
+++ funciones_1_filter.py
@@ -28,7 +28,14 @@ def precio_con_descuento(precio):
     if precio > 50 and precio < 100:
         return True
 
+# Precios sin IVA
 precios_sin_iva = [3, 148, 74, 71, 4, 83, 95, 20, 61, 10, 69, 67, 23, 164, 97, 67, 144, 200, 38, 90, 200, 162, 6, 180, 65, 71, 90, 182, 16, 132, 182, 108, 90, 196, 48, 2, 158, 88, 39, 39, 54, 80, 89, 3, 90, 170, 88, 71, 142, 45, 81, 194, 36, 39, 30, 33, 38, 44, 134, 43, 12, 52, 170, 162, 192, 83, 18, 176, 120, 28, 86, 188, 51, 11, 96, 13, 198, 34, 66, 23, 200, 62, 194, 91, 51, 26, 152, 186, 86, 38, 46, 66, 83, 66, 40, 2, 20, 12, 91, 53]
+
+# Map para agregar el IVA a cada elemento de precios_sin_iva
 precios_con_iva = list(map(precio_con_iva, precios_sin_iva))
+
+# Filter, solo elementos de precios_con_iva que tengan descuento
 precios_con_descuento = list(filter(precio_con_descuento, precios_con_iva))
-print(precios_con_descuento)
+
+porcentaje_productos_candidatos_descuento = (len(precios_con_descuento) / len(precios_sin_iva)) * 100
+print(f'{round(porcentaje_productos_candidatos_descuento)} %')
